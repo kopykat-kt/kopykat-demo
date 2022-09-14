018ba43e5cd98f5663015c895fa29a261f6bc66f
Alejandro Serrano <trupill@gmail.com>
2022-09-14 20:27:07 +0200

    Initial commit, showcase 0.1

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ksp)
+    idea
+}
+
+group = "fp.serrano"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+}
+
+dependencies {
+    ksp(libs.kopyKat)
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+kotlin {
+    sourceSets.main {
+        kotlin.srcDir("build/generated/ksp/main/kotlin")
+    }
+    sourceSets.test {
+        kotlin.srcDir("build/generated/ksp/test/kotlin")
+    }
+}
+
+idea {
+    module {
+        // Not using += due to https://github.com/gradle/gradle/issues/8749
+        sourceDirs = sourceDirs + file("build/generated/ksp/main/kotlin") // or tasks["kspKotlin"].destination
+        testSourceDirs = testSourceDirs + file("build/generated/ksp/test/kotlin")
+        generatedSourceDirs = generatedSourceDirs + file("build/generated/ksp/main/kotlin") + file("build/generated/ksp/test/kotlin")
+    }
+}
---
+++ src/main/kotlin/fp/serrano/kopykat/example/Person.kt
@@ -0,0 +1,9 @@
+package fp.serrano.kopykat.example
+
+data class Person(val name: String, val age: Int)
+
+fun main() {
+  val p1 = Person("Alex", 1)
+  val p2 = p1.copy { age++ }
+  println(p2.age)
+}
